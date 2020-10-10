e4723e746dbf657982b561ad999e6de1d6b27bed
Val Nuñez <val@friendlyhello.com>
2020-10-10 22:10:35 +0000

    Working on a classes exercise.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main()
+class Dog
+{
+  
+  public:
+
+  // Constructor (a function that gets called when an object is made)
+  Dog()
+  {
+    cout << "WAU WAU" << endl;
+  }
+
+  // Member variables
+  string Name;
+  int Health;
+  float Age;
+
+
+  // void Bark()
+  // {
+  //   cout << "Waff waff" << endl;
+  // }
+};
+
+int main ()
 {
 
-  cout << " --- Pointers Practice! --- " << endl;
+  // Objects made from the class Dog
+  Dog Mog;
+  Dog Bobo;
 
-  // declare a variable with some value
-  int heal = 50;
-
-  // create a pointer of type int
-  int* healPointer;
-
-  // initialize the pointer with an address
-  healPointer = &heal;
-
-  // print out the address and the variable stored in that address
-
-  // address of healPointer
-  cout << "The address healPointer points to is: " << healPointer << endl;
-
-  // variable stored in the address of healPointer
-  // placing the * before the pointer variable name is called "dereferencing"
-  cout << "The pointer healPointer now points to the address that stores the variable heal: " << *healPointer << endl;
-
-  // Assign another variable to the address of healPointer
-
-  // dereferencing healPointer to hold a new value - notice * is now on the left side 
-  // of healPointer
-  *healPointer = 100;
-
-  // print out life variable from the address of healPointer
-  cout << "The pointer healPointer now has a new value of: "  << *healPointer << endl;
 }
