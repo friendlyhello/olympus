cdfdf1a861e035431c3d8b947e80d08609cc7c43
Val Nuñez <val@friendlyhello.com>
2020-10-11 01:17:24 +0000

    Added struct exercise.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-struct Container
+struct Player
 {
 
   string Name;
 
-  int X;
-  int Y;
-  int Z;
+  int Level;
+  int Health;
+  float Damage;
+  float Stamina;
+
+  void TakeDamage(float dmg)
+  {
+    Health -= dmg;
+  }
+
+  int GetLevel()
+  {
+    return Level;
+  }
 
 };
 
-int main()
+int main ()
 {
-// create new struct of type Container
-
-// initilize container struct in a list
-Container container = {"Mog", 5, 10, 15}; 
-
-// create Pointer of type Container
-// PtrToCont is now initilaized for the variable address &container
-Container* PtrToCont = &container;
-
-// access struct data with Pointer
-
-cout << (*PtrToCont).Name << endl;
-
-// using "arrow notation" - syntactic sugar
-cout << PtrToCont->Name << endl;
 
 }
