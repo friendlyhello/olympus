4057b4d0eb2d6876114d07d0f1829baffce2658a
Val Nuñez <val@friendlyhello.com>
2020-10-11 18:36:10 +0000

    Example 1 of Constructors added.

--- main.cpp
+++ main.cpp
@@ -1,87 +1,50 @@
 #include <iostream>
 using namespace std;
 
-// Second struct for nesting
-struct Location
-{
-  float X;
-  float Y;
-  float Z;
-};
+// Constructors are the functions that get called when an object is created!
 
-// Struct Definition (custom datatype)
-struct Player
+class Dog
 {
-  // NOTE: It is not recommended to access the member variables directly within a struct
-  //       using dot notatation.
-  //       It is best to use FUNCTIONS, as seen below with TakeDamage() and GetLevel() 
+  public:
+  
+  // Dog() is the constructor, the function that gets called when a Dog object is made
+
+  // You can call a function from within a constructor
+  Dog()
+  {
+    Bark();
+
+    // You can also define member variables within the constructor
+    Name = "Shadow";
+    Age = 12.f;
+    Health = 100;
+  }
+  
   string Name;
-
-  int Level;
+  float Age;
   int Health;
-  float Damage;
-  float Stamina;
 
-  // second nested struct
-  Location location = {0.f, 0.f, 0.f};
-
-  void TakeDamage(float dmg)
+  void Bark()
   {
-    Health -= dmg;
-  }
-
-  int GetLevel()
-  {
-    if(Level >= 10)
-      {
-        cout << "Player Level is greater than 10! \n";
-      }
-
-    return Level;
-  }
-
-  // second nested struct function
-  void DisplayLocation()
-  {
-    cout << "Location X: " << location.X << endl;
-    cout << "Location Y: " << location.Y << endl;
-    cout << "Location Z: " << location.Z << endl;
+    cout << "woof woof" << endl;
   }
 };
 
-int main ()
+int main()
 {
-  // define struct of type player
-  Player playerOne;
+  Dog Mog;
 
-  // define Player variables
-  playerOne.Name = "Celes";
-  playerOne.Level = 12;
-  playerOne.Health = 100;
-  playerOne.Damage = 35.5f;
-  playerOne.Stamina = 10.5f;
+  cout << "Dog object created: " << Mog.Name << endl;
+  cout << "Dog object age: " << Mog.Age << endl;
+  cout << "Dog object health: " << Mog.Health << endl;
 
-  // Get Level with a function
-  // BE MINDFUL OF "FLOW OF CONTROL" HERE
-  cout << "Player One Level is: " << playerOne.GetLevel() << endl;
+  // You can also define new values for existing member variables
 
-  // Call function to apply damage
-  playerOne.TakeDamage(10.f);
+  Mog.Name = "Cid";
+  Mog.Age = 1.5f;
+  Mog.Health = 50;
 
-  // Print damage taken from calling  function
-  cout << "Player One takes " << 10.f << " damage!" << endl;
-
-  // Print Health after taking damage
-  cout << "Player One's HEALTH is now " << playerOne.Health << endl;
-
-  // Print second nested struct function DisplayLocation()
-  playerOne.DisplayLocation();
-
-  // A new struct, declared on the same line, including the nested location struct in its
-  // own initializer list 
-  Player playerTwo = {"Locke", 12, 100, 45.5f, 7.f, {10.f, 100.f, 535.f}};
-
-  // Print Player Two's location
-  cout << "Player Two: " << playerTwo.Name << " is located at:" << endl; 
-  playerTwo.DisplayLocation(); 
+  cout << "Dog object created with new value: " << Mog.Name << endl;
+  cout << "Dog object age with new value: " << Mog.Age << endl;
+  cout << "Dog object health with new value: " << Mog.Health << endl;
 }
