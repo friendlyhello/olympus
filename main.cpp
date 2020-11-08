02d066268c333ff806d5da943ac7252a24a83cd2
Val Nuñez <val@friendlyhello.com>
2020-11-08 21:14:19 +0000

    quick save. will save often because this new shit makes me so nervous.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // -- Dynamic Variables Exercise --
 
 // Using new, delete and nullptr keywords
 
+//struct
+struct Character 
+{
+  string Name;
+  float Health;
+};
+
 int main()
 {
+//create pointer of type Character struct
+Character* PtrToChar = new Character();
+
+// NOTICE new Character struct LOOKS LIKE A FUNCTION^^^
 
 }
 
