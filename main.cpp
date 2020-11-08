ab71065cc2385321b5e74f0db2c3d2c4e95c3ba5
Val Nuñez <val@friendlyhello.com>
2020-11-08 21:48:42 +0000

    Practicing with the 'delete' keyword.

--- main.cpp
+++ main.cpp
@@ -17,10 +17,17 @@ int main()
 {
 //create pointer of type Character struct
 Character* PtrToChar = new Character();
+// This pointer to Character struct (PtrToChar) is now allocated to the -heap-
 
-// NOTICE new Character struct LOOKS LIKE A FUNCTION^^^
+// NOTICE 'new Character();' struct LOOKS LIKE A FUNCTION. It essentially is. It is also a (default) constructor which automatically holds values of 0.
+
+PtrToChar->Name = "Mog";
+
+// I am responsible for memory allocation. If creating a new dynamic value for the heap, I must also delete it using the 'delete' keyword.
+
+
 
 }
 
-// function definitions
+
 
