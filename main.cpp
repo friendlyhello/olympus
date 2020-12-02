13b2a7860c87516180bffe5bb67360699b889d0c
Val Nuñez <val@friendlyhello.com>
2020-12-02 06:34:44 +0000

    Added exercise for casting!

--- main.cpp
+++ main.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
 using namespace std;
 
-//Parent Class
+// CASTING! 
+
+// Derivation chain:
+// Parent
 class Object
 {
-  public:
+public:
   virtual void BeginPlay();
+
+  // Add a function to test casting:
+  void ObjectFunction()
+  {
+    cout << "ObjectFunction() called!\n\n";
+  }
 };
 
-//Child Class
+// Child
 class Actor : public Object
 {
-  public:
+public:
   virtual void BeginPlay() override;
+
+  // Add a function to test casting:
+  void ActorFunction()
+  {
+    cout << "ActionFunction() called!\n\n";
+  }
 };
 
-//Another Child class
-class Pawn : public Actor
+// Grandchild
+class Pawn : public Object
 {
-  public:
+public:
   virtual void BeginPlay() override;
+
+  // Add a function to test casting:
+  void PawnFunction()
+  {
+    cout << "PawnFunction() called!\n\n";
+  }
 };
 
 int main()
 {
-  Object* Ptr_To_Object = new Object;
+
+Object* Ptr_To_Object = new Object;
 
   Actor* Ptr_To_Actor = new Actor;
   
@@ -38,7 +60,31 @@ int main()
 
   for (int i = 0; i < 3; i++)
   {
-    ObjectArrray[i]->BeginPlay();
+    //ObjectArrray[i]->BeginPlay(); (hide this for now)
+
+    // !-- CASTING EXAMPLE STARTS HERE!!! --!
+    // 1. Create temporary new object
+    Object* obj = ObjectArrray[i];
+
+    // 2. Cast to Actor using dynamic_cast function
+    Actor* act = dynamic_cast<Actor*>(obj);
+    // <type that is being converted to>(what is being converted)
+
+    // 3. Perform a check to see whether cast was succesful
+    if (act)
+    {
+      act->ActorFunction();
+    }
+
+    // Now try casting to Pawn!
+    Pawn* pwn = dynamic_cast<Pawn*>(obj);
+
+    // Check to see if it successful!
+    if (pwn)
+    {
+      pwn->PawnFunction();
+    }
+
   }
 
   delete Ptr_To_Object;
@@ -46,7 +92,7 @@ int main()
   delete Ptr_To_Pawn;
 }
 
-//function definition  - remember to "fully qualify"
+// Function definitions - Remember to "fully qualify"
 void Object::BeginPlay()
 {
   cout << "Object begin play!\n";
